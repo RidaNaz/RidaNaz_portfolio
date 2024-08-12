4f2d79726a32643a7119e30f31c7f6dcb60d2c84
RidaNaz <rnaz3414@gmail.com>
2024-08-12 18:17:16 +0500

    Rida naz Portfolio

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,34 +1,3 @@
-export const workExperience = [
-  {
-    id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
-    className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
-  },
-  {
-    id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
-  },
-  {
-    id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp3.svg",
-  },
-  {
-    id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
-    className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
-  },
-];
-
 export const CertificateCards = [
   {
     id: 1,
@@ -240,7 +209,7 @@ export const projects = [
     title: "Cloudinary Gallery Website",
     des: "Gallery Website in which I integrate Cloudinary for efficient image management and optimization...",
     img: "/web3.jpg",
-    iconLists: ["/next.svg", "/re.svg", "/tail.svg", "/ts.svg", "/cd.svg"],
+    iconLists: ["/next.svg", "/sanity.svg", "/tail.svg", "/ts.svg", "/cd.svg"],
     link: "https://its-naz-gallery.vercel.app/",
   },
 ];
