803010320c58bd454a17fb5d7fbf402e03919540
Rida Naz <rnaz3414@gmail.com>
2026-06-01 14:38:04 +0500

    Updated type in sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,11 +1,11 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://www.ridanaz.com",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "monthly" as const,
       priority: 1,
     },
   ];
