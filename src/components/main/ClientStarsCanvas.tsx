90dc6ea3d2b71d6fc5dc7e08d13714145d29f3ea
Rida Naz <rnaz3414@gmail.com>
2026-06-24 14:46:43 +0500

    perf: optimize load speed, accessibility, and fix missing asset errors

---
+++ src/components/main/ClientStarsCanvas.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const StarsCanvas = dynamic(() => import("./StarBackground"), {
+  ssr: false,
+});
+
+export default function ClientStarsCanvas() {
+  return <StarsCanvas />;
+}
